9bb47907ee70fb5b15e6b63d9601f02913ec0d86
code-0y <gagannaik243@gmail.com>
2024-12-15 17:53:08 +0530

    format in email

--- Horner’srule.9.py
+++ Horner’srule.9.py
@@ -1,25 +1,45 @@
-def polynomial_BF(poly,x,n):
- result = 0
- for i in range(n):
-    Sum = poly[i]
- for j in range(n - i - 1):
-     Sum = Sum * x
- result = result + Sum
- print("Value of polynomial 2x3 - 6x2 + 2x - 1 for x = 3 using [BRUTE FORCE method]:",result)
-def horner(poly, x, n):
- res = poly[0]
- for i in range(1, n):
- res = res * x + poly[i]
- print("Value of polynomial 2x3 - 6x2 + 2x - 1 for x = 3 using [HORNER method ] :", 
-res)
-poly = [2, -6, 2, -1]
-x = 3
-n = len(poly)
-start1 = timeit.default_timer()
-polynomial_BF(poly,x,n)
-t1 = timeit.default_timer() - start1
-start2 = timeit.default_timer()
-horner(poly,x,n)
-t2 = timeit.default_timer() - start2
-print("Time complexity of Brute force method O(n2) : ",t1)
-print("Time complexity of Horner method O(n) : ",t2)
+<!DOCTYPE html> 
+<html>
+<head>
+<title>Form Validation</title>
+<h1>Form Validation</h1>
+</head> 
+<body> 
+<form id="myForm"> 
+<label for="name">Name:</label> <input type="text" id="name" name="name"><br><br>  
+<label for="email">Email:</label> <input type="text" id="email" name="email"><br><br>  
+<label for="mobile">Mobile Number:</label> <input type="text" id="mobile" name="mobile"><br><br>
+<label>Gender:</label> <input type="radio" id="male" name="gender" value="Male">Male 
+<input type="radio" id="female" name="gender" value="Female">Female<br><br>
+<label>Hobbies:</label> <input type="checkbox" id="reading" name="hobbies" value="Reading">Reading <input type="checkbox" id="traveling" name="hobbies" value="Traveling">Traveling<br><br>  
+<input type="submit" value="Submit" onclick="countFormElements()"> 
+</form>
+<script> document.getElementById("myForm").addEventListener("submit", function(event) { event.preventDefault();
+//Validation for email format
+ var email = document.getElementById("email").value;   
+var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+ // Validation for mobile number length (assuming 10 characters)
+var mobile = document.getElementById("mobile").value;  
+// Check if any required field is left empty 
+var name = document.getElementById("name").value;  
+if (name === "" || email === "" || mobile === "" ) 
+{ 
+alert("Please fill in all required fields.");
+}
+else if (!emailPattern.test(email)) 
+{
+ alert("Invalid email format. Please enter a valid email address.");   
+}
+else if (mobile.length !== 10 || isNaN(mobile)) 
+{
+ alert("Mobile number should be 10 digits numeric.");
+}
+ else 
+{
+// Form is valid, you can submit it or perform further actions here 
+alert("Form submitted successfully!"); 
+}
+}); 
+</script>
+</body>
+</html
